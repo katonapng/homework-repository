865652ff27cb70c92a68adb3d16c3d8afcc38aa0
Katona <annainterior14@gmail.com>
2021-11-10 12:07:05 +0300

    change iteration method in task02

--- homework00/homework0/task02.py
+++ homework00/homework0/task02.py
@@ -17,7 +17,7 @@ def check_fibonacci(data: Sequence[int]) -> bool:
     elif len(data) == 2 and not (data[0] == 0 and data[1] == 1):
         return False
 
-    for i in range(2, len(data)):
+    for i, _ in enumerate(data):
         if not (data[i] == data[i - 1] + data[i - 2]):
             return False
 
