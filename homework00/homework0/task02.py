34f7ef3756a2bc667d39e6ae1b8d5b18164da8d6
Katona <annainterior14@gmail.com>
2021-11-10 12:34:49 +0300

    correct algorithm in task02 (i forgot to run pytest :()

--- homework00/homework0/task02.py
+++ homework00/homework0/task02.py
@@ -17,7 +17,7 @@ def check_fibonacci(data: Sequence[int]) -> bool:
     elif len(data) == 2 and not (data[0] == 0 and data[1] == 1):
         return False
 
-    for i, _ in enumerate(data):
+    for i, _ in enumerate(data[2:], start=2):
         if not (data[i] == data[i - 1] + data[i - 2]):
             return False
 
