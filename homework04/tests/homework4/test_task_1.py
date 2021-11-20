4128a941127ff93898ee691091a48920baafe5b4
Katona <annainterior14@gmail.com>
2021-11-20 12:10:03 +0300

    remove os usage from test_task_1

--- homework04/tests/homework4/test_task_1.py
+++ homework04/tests/homework4/test_task_1.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 from homework4.task_1_read_file import read_magic_number
 
@@ -7,23 +5,23 @@ from homework4.task_1_read_file import read_magic_number
 def test_positive_case(temp_file):
     """Testing positive case"""
     temp_file.write_text("2\n1234567890\nbdsksn", encoding='utf-8')
-    assert read_magic_number(os.path.abspath(temp_file))
+    assert read_magic_number(temp_file)
 
 
 def test_negative_case(temp_file):
     """Testing negative case"""
     temp_file.write_text("4\n1234567890\nbdsksn", encoding='utf-8')
-    assert not read_magic_number(os.path.abspath(temp_file))
+    assert not read_magic_number(temp_file)
 
 
 def test_value_error_case(temp_file):
     """Testing raise of ValueError"""
     temp_file.write_text("e r t\n1234567890\nbdsksn", encoding='utf-8')
     with pytest.raises(ValueError):
-        read_magic_number(os.path.abspath(temp_file))
+        read_magic_number(temp_file)
 
 
 def test_no_file_case():
     """Testing raise of FileNotFoundError"""
     with pytest.raises(FileNotFoundError):
-        read_magic_number(os.path.abspath("non-existing-file.txt"))
+        read_magic_number("non-existing-file.txt")
