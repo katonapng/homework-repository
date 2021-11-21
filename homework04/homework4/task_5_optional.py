13f96cae31e2f9fd09839105373ade57f18ae799
Katona <annainterior14@gmail.com>
2021-11-21 13:21:03 +0300

    add task_5 from homework4, if i'm not mistaken, all solutions in video contain 'if' structure, so i don't know how to solve it without any 'ifs'

---
+++ homework04/homework4/task_5_optional.py
@@ -0,0 +1,64 @@
+"""
+This task is optional.
+
+Write a generator that takes a number N as an input
+and returns a generator that yields N FizzBuzz numbers*
+Don't use any ifs, you can find an
+approach for the implementation in this video**.
+
+
+Definition of done:
+ - function is created
+ - function is properly formatted
+ - function has tests
+
+
+>>> list(fizzbuzz(5))
+['1', '2', 'fizz', '4', 'buzz']
+
+* https://en.wikipedia.org/wiki/Fizz_buzz
+** https://www.youtube.com/watch?v=NSzsYWckGd4
+"""
+from math import ceil
+from typing import Generator
+
+
+class FizzBuzz():
+    def __init__(self, n) -> None:
+        self.fizz = (['']*2 + ['fizz'])*(ceil(n/3)) + ['']
+        self.buzz = (['']*4 + ['buzz'])*(ceil(n/5))
+        self.numbers = [str(i) for i in range(1, n + 1)]
+
+
+def fizzbuzz_generator(n: int, fb: FizzBuzz):
+    for i in range(0, n):
+        yield fb.fizz[i] + fb.buzz[i] if fb.buzz[i] + fb.fizz[i] \
+              else fb.numbers[i]
+
+
+def fizzbuzz(n: int) -> Generator[str, None, None]:
+    """Return return generator for FizzBuzz numbers, n is an exact integer
+
+    >>> list(fizzbuzz(5))
+    ['1', '2', 'fizz', '4', 'buzz']
+
+    >>> list(fizzbuzz(15))
+    ['1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', \
+'buzz', '11', 'fizz', '13', '14', 'fizzbuzz']
+
+    >> list(fizzbuzz(0))
+    []
+
+    >>> list(fizzbuzz(-1))
+    []
+
+    >>> list(fizzbuzz(1.2))
+    Traceback (most recent call last):
+        ...
+    ValueError: n must be an integer
+    """
+    if not isinstance(n, int):
+        raise ValueError("n must be an integer")
+    fb_class = FizzBuzz(n)
+    fb_generator = fizzbuzz_generator(n, fb_class)
+    return fb_generator
